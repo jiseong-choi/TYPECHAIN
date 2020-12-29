3d45debffa33dda6786713e44af7a0a12b7e70bb
jiseong-choi <jiseong1951@kakao.com>
2020-12-29 17:19:44 +0900

    saved

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-console.log("hello");
-console.log("hello");
-console.log("hello");
+"use strict";
+/*
+Typescript
+typescript is superset of javascript
+You need to declare the type of values, returns so on.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'jiseong-choi', age = 19, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name},you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
-console.log("hello")
-console.log("hello")
-console.log("hello")
+/*
+Typescript 
+typescript is superset of javascript
+You need to declare the type of values, returns so on.
+*/
+
+const name = 'jiseong-choi',
+    age = 19,
+    gender = 'male';
+
+const sayHi = (name,age,gender) => {
+    console.log(`Hello ${name},you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name,age,gender)
+
+export { };
