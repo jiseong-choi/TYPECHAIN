0f16f9560247d54be8e95cac1cdd40205f4d5811
jiseong-choi <jiseong1951@kakao.com>
2020-12-29 21:43:29 +0900

    123

--- dist/index.js
+++ dist/index.js
@@ -21,5 +21,10 @@ If remove the ? of the parameter it will not run.
 That's why we using Typescript.
 These functions make the results of your code more predictable.
 */
+/*
+Now it is a tip for Typescript. tsc-watch module is good for development tool.
+It keep watching ur code and if it changes then recompile the code.
+npm install tsc-watch --save --dev or yarn add tsc-watch --dev
+*/
 console.log(text);
 //# sourceMappingURL=index.js.map
