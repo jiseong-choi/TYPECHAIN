571a4b5721c411b567f63069e35e47063e3ee039
jiseong-choi <jiseong1951@kakao.com>
2020-12-29 19:32:17 +0900

    wow

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ typescript is superset of javascript
 You need to declare the type of values, returns so on.
 */
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = 'jiseong-choi', age = 19, gender = 'male';
 const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name},you are ${age}, you are a ${gender}`);
+    /*
+    In typescript if we don't declare the type of the value
+    they throw the warning to me like this
+    "Parameter 'name' implicitly has an 'any' type, but a better type may be inferred from usage."
+    It is benefit of using Typescript. We can declare the type of the value like this "name:string".
+    We can also declare the type of return.
+    */
+    return `Hello ${name},you are ${age}, you are a ${gender}`;
 };
-sayHi(name, age);
+const text = sayHi("jiseong-choi", 19);
+/*
+If remove the ? of the parameter it will not run.
+That's why we using Typescript.
+*/
+console.log(text);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -4,19 +4,23 @@ typescript is superset of javascript
 You need to declare the type of values, returns so on.
 */
 
-const sayHi = (name, age, gender?) => {
+const sayHi = (name:string, age:number, gender?:string):string => {
     /*
     In typescript if we don't declare the type of the value 
     they throw the warning to me like this
     "Parameter 'name' implicitly has an 'any' type, but a better type may be inferred from usage."
+    It is benefit of using Typescript. We can declare the type of the value like this "name:string".
+    We can also declare the type of return.
     */
-    console.log(`Hello ${name},you are ${age}, you are a ${gender}`)
+    return `Hello ${name},you are ${age}, you are a ${gender}`
 }
 
-sayHi("jiseong-choi", 19)
+const text = sayHi("jiseong-choi", 19)
 /*
 If remove the ? of the parameter it will not run. 
 That's why we using Typescript.
 */
+
+console.log(text)
 
 export { };
