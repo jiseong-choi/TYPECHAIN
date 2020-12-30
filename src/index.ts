71d8144e7304096f17b3f364c366e7cc35893996
jiseong-choi <jiseong1951@kakao.com>
2020-12-30 14:52:59 +0900

    saved

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
